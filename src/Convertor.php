d6f54b76ed078dc320576c11e08b035eb3a76672
Khizhenok Pavel <khizhenokp@gmail.com>
2024-10-30 23:54:06 +0300

    init v1.0.0

---
+++ src/Convertor.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Pavelkhizhenok\Convertor;
+
+class Convertor {
+
+    protected array $availableExchangeRates = array();
+
+    public function __construct(array $availableExchangeRates) {
+
+        $this->setAvailableExchangeRates($availableExchangeRates);
+
+    }
+
+    public function convert(float $sum, string $from, string $to): float|bool {
+
+        $rate = $this->getRate($from, $to);
+
+        if (empty($rate)) {
+            return false;
+        }
+
+        return $rate * $sum;
+
+    }
+
+    public function getRate(string $from, string $to): float|bool {
+
+        if (empty($this->availableExchangeRates[$from])) {
+            return false;
+        }
+
+        $currencyRates = $this->availableExchangeRates[$from];
+
+        if (empty($currencyRates[$to])) {
+            return false;
+        }
+
+        return $currencyRates[$to];
+
+    }
+
+    public function setAvailableExchangeRates(array $availableExchangeRates) {
+
+        $this->availableExchangeRates = $availableExchangeRates;
+
+    }
+
+}
---
+++ src/FreeCurrencyApiClient.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Pavelkhizhenok\Convertor;
+
+use Pavelkhizhenok\SupportClasses\Classes\ApiClient;
+
+class FreeCurrencyApiClient extends ApiClient {
+
+    protected string $apiKey = '';
+
+    public function __construct(string $apiKey) {
+
+        $this->apiKey = $apiKey;
+
+        parent::__construct($this->getConfig());
+
+    }
+
+    public function getConfig(): array {
+
+        return array(
+            'url' => 'api.freecurrencyapi.com/v1/',
+            'params' => array(
+                'apikey' => $this->apiKey,
+            ),
+        );
+
+    }
+
+    public function getLatestExchangeRates(string $baseCurrency = '', array $currencies = array()): array|bool {
+
+        return $this->sendRequest('latest', 
+            array(
+                'base_currency' => $baseCurrency,
+                'currencies' => $this->getCurrenciesList($currencies),
+            )
+        );
+
+    }
+
+    public function getCurrencies(array $currencies = array()): array|bool {
+
+        return $this->sendRequest('currencies',
+            array(
+                'currencies' => $this->getCurrenciesList($currencies),
+            )
+        );
+
+    }
+
+    public function getHistoricalExchangeRates(string $date, string $baseCurrency = '', array $currencies = array()): array|bool {
+
+        return $this->sendRequest('historical', 
+            array(
+                'date' => $date,
+                'base_currency' => $baseCurrency,
+                'currencies' => $this->getCurrenciesList($currencies),
+            )
+        );
+
+    }
+
+    public function getExchangeRates(array $currencies = array()) {
+
+        $rates = array();
+
+        foreach ($currencies as $currency) {
+            $exchangeRates = $this->getLatestExchangeRates($currency, $currencies);
+
+            if ($exchangeRates) {
+                $rates[$currency] = $exchangeRates['response'];
+            }
+        }
+
+        return $rates;
+
+    }
+
+    protected function getCurrenciesList(array $currencies): string {
+        
+        if (!empty($currencies)) {
+            return implode(',', $currencies);
+        }
+
+        return '';
+
+    }
+
+    public function getResponseByJson(string $response): array|bool {
+
+        $response = parent::getResponseByJson($response);
+
+        if (!isset($response['data'])) {
+            return false;
+        }
+
+        return $response['data'];
+
+    }
+
+}
